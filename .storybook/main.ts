f7f94b4f71d9367ecdc7931e0314cc81d03f191d
Tunde Adekoya <adekoyagbolahan101@gmail.com>
2025-06-17 14:40:36 +0100

    Updated: Storybook main configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,20 +4,23 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 const config: StorybookConfig = {
   "stories": [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
   "addons": [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-docs",
   ],
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
   },
-  "docs": {
-    "autodocs": "tag",
-  },
+  docs: {
+    autodocs: "tag"
+  } as any, 
+
   viteFinal: async (config) => {
     config.plugins?.push(
       tsconfigPaths({
@@ -27,6 +30,7 @@ const config: StorybookConfig = {
     return config;
   }
 };
+
 export default config;
 
 
